89b07726c62b34bba61e8bce800c0d9f71326b3a
Bartolome Sanchez <bartolome.sanchez@ntt.eu>
2017-03-11 16:04:04 +0100

    Add both Term and Definition initial models

--- lengcol/definitions/models.py
+++ lengcol/definitions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Term(models.Model):
+    value = models.CharField(max_length=255)
+
+
+class Definition(models.Model):
+    term = models.ForeignKey(Term, on_delete=models.CASCADE)
+    value = models.TextField()
