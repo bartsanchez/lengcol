67b331876399c5fe54968e79f7159970f6b30a29
Bartolome Sanchez <bartolome.sanchez@ntt.eu>
2017-03-11 18:31:16 +0100

    Add setup.py file
    Configured with setuptools.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name='lengcol',
+    description='Lenguaje Coloquial project',
+
+    version='0.1',
+
+    author='Bartolome Sanchez',
+    author_email='bsanchezsalado@gmail.com',
+
+    url='http://www.lenguajecoloquial.com/',
+
+    packages=(setuptools.find_packages()),
+)
