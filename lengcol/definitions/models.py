995dbdbc4ad5a670e74606d06e30d15e112fcaee
Bartolome Sanchez <bartolome.sanchez@ntt.eu>
2017-03-11 16:09:17 +0100

    Add __str__ methods to models

--- lengcol/definitions/models.py
+++ lengcol/definitions/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Term(models.Model):
     value = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.value
+
 
 class Definition(models.Model):
     term = models.ForeignKey(Term, on_delete=models.CASCADE)
     value = models.TextField()
+
+    def __str__(self):
+        return self.value
