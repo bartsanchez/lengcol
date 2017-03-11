2c8f04101188b9dd4b71a02d285b3496a02c5264
Bartolome Sanchez <bartolome.sanchez@ntt.eu>
2017-03-11 16:32:00 +0100

    Add testing for current models

--- lengcol/definitions/tests.py
+++ lengcol/definitions/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django import test
 
-# Create your tests here.
+from definitions import factories
+
+
+class TermTests(test.TestCase):
+    def test_str(self):
+        term = factories.TermFactory(value='my fake term')
+
+        self.assertEqual(str(term), 'my fake term')
+
+
+class DefinitionTests(test.TestCase):
+    def test_str(self):
+        definition = factories.DefinitionFactory(value='my fake definition')
+
+        self.assertEqual(str(definition), 'my fake definition')
