1cca22d78fd266bbd341f6eb8c0a9a4ada455ecf
Bartolome Sanchez <bartolome.sanchez@ntt.eu>
2017-03-14 20:04:04 +0100

    Add lenguajecoloquial domains to ALLOWED_HOSTS

--- lengcol/lengcol/settings/prod.py
+++ lengcol/lengcol/settings/prod.py
@@ -5,7 +5,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['lengcol.herokuapp.com']
+ALLOWED_HOSTS = [
+    'lengcol.herokuapp.com',
+    'www.lenguajecoloquial.com',
+    'www.lenguajecoloquial.es',
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
