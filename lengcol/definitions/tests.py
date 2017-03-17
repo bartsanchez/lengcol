ecc85e3ec0578aae69353fa1d490760f2d366348
Bartolome Sanchez <bartolome.sanchez@ntt.eu>
2017-03-17 20:57:14 +0100

    Add base HTML template

--- lengcol/definitions/tests.py
+++ lengcol/definitions/tests.py
@@ -15,3 +15,18 @@ class DefinitionTests(test.TestCase):
         definition = factories.DefinitionFactory(value='my fake definition')
 
         self.assertEqual(str(definition), 'my fake definition')
+
+
+class IndexViewTests(test.TestCase):
+    def setUp(self):
+        self.client = test.Client()
+
+    def test_template_extends(self):
+        response = self.client.get('/')
+
+        self.assertTemplateUsed(response, 'lengcol/base.html')
+
+    def test_title(self):
+        response = self.client.get('/')
+
+        self.assertContains(response, '<h1>Lenguaje coloquial</h1>', html=True)
