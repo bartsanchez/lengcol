bf52fc438cd086928ed176794d8930dc561673f6
Bartolome Sanchez <bartolome.sanchez@ntt.eu>
2017-03-18 23:48:23 +0100

    Set "value" field for Term model as unique

--- lengcol/definitions/models.py
+++ lengcol/definitions/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Term(models.Model):
-    value = models.CharField(max_length=255)
+    value = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.value
