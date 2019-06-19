b7dc5aa5e86ec72b4447ce6d536664f7ef5446bf
Bartolome Sanchez Salado <aliaselbarto@gmail.com>
2019-06-19 23:23:06 +0200

    Use bootstrap cards in index view for terms

--- lengcol/definitions/tests/tests_views.py
+++ lengcol/definitions/tests/tests_views.py
@@ -24,13 +24,14 @@ class IndexViewTests(test.TestCase, mixins.W3ValidatorMixin):
 
     def test_has_link_to_term_detail(self):
         term = factories.TermFactory(value='my fake term')
-
+        definition = factories.DefinitionFactory(term=term,
+                                                 value='fake definition')
         response = self.client.get(self.url)
 
         self.assertContains(
             response,
             '<a href="{}">my fake term</a>'.format(
-                reverse('term-detail', kwargs={'slug': term.slug})
+                reverse('term-detail', kwargs={'slug': definition.term.slug})
             ),
             html=True
         )
