8fdee26e82fc17fa1acedd9862fc79298d47b0fa
Bartolomé Sánchez Salado <bartolome.sanchez@qustodio.com>
2019-08-19 17:18:58 +0200

    Add active fields to Base Model

--- lengcol/base/models.py
+++ lengcol/base/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class BaseModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
     class Meta:
         abstract = True
