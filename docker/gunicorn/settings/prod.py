b5559e7a6b3698c19718bfaddb47a2e14caa1400
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2020-04-10 16:14:29 +0200

    Configure Gunicorn workers and threads in prod

--- docker/gunicorn/settings/prod.py
+++ docker/gunicorn/settings/prod.py
@@ -1,3 +1,8 @@
-bind = "0.0.0.0:8000"
+bind = '0.0.0.0:8000'
+
+workers = 3
+worker_class = 'gthread'
+threads = 3
+
 accesslog = '/var/log/gunicorn/access.log'
 errorlog = '/var/log/gunicorn/error.log'
