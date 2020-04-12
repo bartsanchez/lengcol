c9d832add1786400da674763bdaaa64d9a1047b5
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2020-04-12 18:28:47 +0200

    Use pytest-recording to W3 validator tests

--- lengcol/base/mixins.py
+++ lengcol/base/mixins.py
@@ -1,9 +1,11 @@
+import pytest
 import requests
 
 W3_VALIDATOR_URL = 'https://validator.w3.org/nu/'
 
 
 class W3ValidatorMixin:
+    @pytest.mark.vcr
     def test_is_w3_valid(self):
         index_web = self.client.get(self.url)
 
