a6c5c2f4e1c7339d7e35c6c12fee4deb1d46f1f6
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2020-09-14 21:43:40 +0200

    Add TermGathering service

--- lengcol/definitions/services.py
+++ lengcol/definitions/services.py
@@ -1,13 +1,13 @@
 from definitions import models
 
 
-class DefinitionGathering:
+class ObjectGathering:
     @classmethod
     def _objects(cls):
-        return models.Definition.objects
+        return cls.model.objects
 
     @classmethod
-    def _get_definitions(cls, order_by=None, items=None):
+    def _get_objects(cls, order_by=None, items=None):
         objects = cls._objects()
         if order_by:
             objects = objects.order_by(order_by)
@@ -15,5 +15,29 @@ class DefinitionGathering:
         return objects.all()[:items]
 
     @classmethod
+    def _get_last_objects(cls, items=5):
+        return cls._get_objects(order_by='-created', items=items)
+
+
+class DefinitionGathering(ObjectGathering):
+    model = models.Definition
+
+    @classmethod
+    def _get_definitions(cls, order_by=None, items=None):
+        return cls._get_objects(order_by, items)
+
+    @classmethod
     def get_last_definitions(cls, items=5):
-        return cls._get_definitions(order_by='-created', items=items)
+        return cls._get_last_objects(items=items)
+
+
+class TermGathering(ObjectGathering):
+    model = models.Term
+
+    @classmethod
+    def _get_terms(cls, order_by=None, items=None):
+        return cls._get_objects(order_by, items)
+
+    @classmethod
+    def get_last_terms(cls, items=5):
+        return cls._get_last_objects(items=items)
---
+++ lengcol/definitions/tests/services/tests_term_gathering.py
@@ -0,0 +1,54 @@
+from django import test
+
+from definitions import factories, services
+
+
+class TermGatherinTests(test.TestCase):
+    def setUp(self):
+        self.service = services.TermGathering
+        self.fake_term_1 = factories.TermFactory(value='l')
+        self.fake_term_2 = factories.TermFactory(value='o')
+        self.fake_term_3 = factories.TermFactory(value='e')
+        self.objects = [self.fake_term_1, self.fake_term_2, self.fake_term_3]
+
+    def test_get_terms(self):
+        self.assertListEqual(
+            list(self.service._get_terms()),
+            self.objects,
+        )
+
+    def test_get_terms__order__asc(self):
+        self.assertListEqual(
+            list(self.service._get_terms(order_by='value')),
+            [self.fake_term_3, self.fake_term_1, self.fake_term_2],
+        )
+
+    def test_get_terms__order__desc(self):
+        self.assertListEqual(
+            list(self.service._get_terms(order_by='-value')),
+            [self.fake_term_2, self.fake_term_1, self.fake_term_3],
+        )
+
+    def test_get_terms__items__less_than_total(self):
+        self.assertListEqual(
+            list(self.service._get_terms(items=2)),
+            [self.fake_term_1, self.fake_term_2],
+        )
+
+    def test_get_terms__items__equal_than_total(self):
+        self.assertListEqual(
+            list(self.service._get_terms(items=3)),
+            self.objects,
+        )
+
+    def test_get_terms__items__more_than_total(self):
+        self.assertListEqual(
+            list(self.service._get_terms(items=4)),
+            self.objects,
+        )
+
+    def test_get_last_terms(self):
+        self.assertListEqual(
+            list(self.service.get_last_terms(items=2)),
+            [self.fake_term_3, self.fake_term_2],
+        )
