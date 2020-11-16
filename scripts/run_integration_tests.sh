d92b0536746709012b604df7b59d260b777caa0a
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2020-11-16 22:25:50 +0100

    Make integration tests script fail on tests failures

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+set -o errexit
 set -o pipefail
 set -o nounset
 
 export ENV=test
 
+# Prepare system
 make build
 make start START_SERVICES="web run_migrations"
 sleep 5
+
+# Run tests
+set +o errexit
 make run RUN_SERVICE=test
+return_code=$?
+set -o errexit
+
+# Down system
 make stop
+
+# Exit script
+exit $return_code
