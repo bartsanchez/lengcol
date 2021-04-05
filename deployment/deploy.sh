8e075dacc18df73038a7525590d5be3a2da48b6f
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2021-04-05 12:22:24 +0200

    Update deploy script to use envvar for Vault password

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -8,4 +8,6 @@ CURRENT_PATH="${BASH_SOURCE%/*}"
 HOSTS_FILE="${CURRENT_PATH}/ansible/hosts"
 DEPLOY_FILE="${CURRENT_PATH}/ansible/deploy.yml"
 
-ansible-playbook -i ${HOSTS_FILE} ${DEPLOY_FILE} --private-key deployment/aws_key_pair.pem --ask-vault-pass
+echo $VAULT_PASSWORD > ./vault_password_file.txt
+
+ansible-playbook -i ${HOSTS_FILE} ${DEPLOY_FILE} --private-key deployment/aws_key_pair.pem --vault-password-file=./vault_password_file.txt
