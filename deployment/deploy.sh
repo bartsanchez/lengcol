bbc10c1d3010520a91b1c5badef4613ae8ace49a
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2021-04-06 22:48:14 +0200

    Add AWS key pair pem file from envvar

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -10,4 +10,7 @@ DEPLOY_FILE="${CURRENT_PATH}/ansible/deploy.yml"
 
 echo $VAULT_PASSWORD > ./vault_password_file.txt
 
-ansible-playbook -i ${HOSTS_FILE} ${DEPLOY_FILE} --private-key deployment/aws_key_pair.pem --vault-password-file=./vault_password_file.txt
+printf '%s\n' "$AWS_KEY_PAIR_PEM_FILE" > ./aws_key_pair.pem
+chmod 600 ./aws_key_pair.pem
+
+ansible-playbook -i ${HOSTS_FILE} ${DEPLOY_FILE} --private-key ./aws_key_pair.pem --vault-password-file=./vault_password_file.txt
