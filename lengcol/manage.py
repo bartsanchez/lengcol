69a2105bb49ee42333909cceed99d0e960a630c2
Bartolomé Sánchez Salado <bartolome.salado@kiwi.com>
2023-12-24 11:40:18 +0100

    Update manage.py script to newer version

--- lengcol/manage.py
+++ lengcol/manage.py
@@ -2,24 +2,20 @@
 import os
 import sys
 
-if __name__ == "__main__":
+
+def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lengcol.settings.base")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django  # noqa: F401
-        except ImportError:
-            msg = (
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-            raise ImportError(
-                msg,
-            )
-        raise
+    except ImportError as exc:
+        msg = (
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        )
+        raise ImportError(msg) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
