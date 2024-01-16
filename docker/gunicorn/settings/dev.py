a48716f414601fc0911cb7f2f64d0d3f3965e0e3
Bartolomé Sánchez Salado <bartolome.salado@kiwi.com>
2024-01-16 17:57:16 +0100

    Switch Gunicorn worker_class to gevent instead of gthread

--- docker/gunicorn/settings/dev.py
+++ docker/gunicorn/settings/dev.py
@@ -1,8 +1,7 @@
 bind = "0.0.0.0:8000"
 
-workers = 1
-worker_class = "gthread"
-threads = 1
+worker_class = "gevent"
+workers = 3
 
 accesslog = "/var/log/gunicorn/access.log"
 errorlog = "/var/log/gunicorn/error.log"
