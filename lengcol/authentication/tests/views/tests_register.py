6399a14f2a9fe292a1c39f845d2f2aa825110b55
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2024-05-25 21:20:29 +0200

    Fix Ruff rule PLR0913 (too-many-arguments)
    https://docs.astral.sh/ruff/rules/too-many-arguments/

--- lengcol/authentication/tests/views/tests_register.py
+++ lengcol/authentication/tests/views/tests_register.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import splinter
 from base import mixins
 from django import test
@@ -5,6 +7,14 @@ from django.contrib import auth
 from django.urls import reverse
 
 from authentication import factories
+
+
+@dataclass
+class UserInfo:
+    username: str
+    password1: str
+    password2: str
+    email: str
 
 
 class RegisterViewTests(
@@ -19,15 +29,23 @@ class RegisterViewTests(
         "Página de registro de usuarios para el proyecto Lenguaje Coloquial."
     )
 
+    def setUp(self):
+        self.user_info = UserInfo(
+            username="fake_user",
+            password1="fake_password",
+            password2="fake_password",
+            email="foo@bar.qux",
+        )
+
     @classmethod
     def setUpTestData(cls):
         cls.url = reverse("register")
 
-    def fill_and_send_form(self, browser, username, password1, password2, email):
-        browser.fill("username", username)
-        browser.fill("password1", password1)
-        browser.fill("password2", password2)
-        browser.fill("email", email)
+    def fill_and_send_form(self, browser, user_info: UserInfo):
+        browser.fill("username", user_info.username)
+        browser.fill("password1", user_info.password1)
+        browser.fill("password2", user_info.password2)
+        browser.fill("email", user_info.email)
         browser.find_by_id("register-form").click()
 
     def test_register_view(self):
@@ -36,13 +54,7 @@ class RegisterViewTests(
 
             self.assertEqual(auth.get_user_model().objects.count(), 0)
 
-            self.fill_and_send_form(
-                browser=browser,
-                username="fake_user",
-                password1="fake_password",
-                password2="fake_password",
-                email="foo@bar.qux",
-            )
+            self.fill_and_send_form(browser=browser, user_info=self.user_info)
 
             self.assertEqual(auth.get_user_model().objects.count(), 1)
 
@@ -62,13 +74,7 @@ class RegisterViewTests(
 
             self.assertEqual(auth.get_user_model().objects.count(), 1)
 
-            self.fill_and_send_form(
-                browser=browser,
-                username="fake_user",
-                password1="fake_password",
-                password2="fake_password",
-                email="foo@bar.qux",
-            )
+            self.fill_and_send_form(browser=browser, user_info=self.user_info)
 
             self.assertEqual(auth.get_user_model().objects.count(), 1)
 
@@ -80,12 +86,6 @@ class RegisterViewTests(
         with splinter.Browser("django") as browser:
             browser.visit(self.url)
 
-            self.fill_and_send_form(
-                browser=browser,
-                username="fake_user",
-                password1="fake_password",
-                password2="fake_password",
-                email="foo@bar.qux",
-            )
+            self.fill_and_send_form(browser=browser, user_info=self.user_info)
 
             self.assertIn("Usuario: fake_user", browser.html)
