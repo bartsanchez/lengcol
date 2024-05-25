4b73841207a927c1be3e567e19a3293d58a89a15
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2024-05-25 21:20:29 +0200

    Fix Ruff rule PLR0913 (too-many-arguments)
    https://docs.astral.sh/ruff/rules/too-many-arguments/

--- lengcol/authentication/tests/tasks/test_new_registered_user.py
+++ lengcol/authentication/tests/tasks/test_new_registered_user.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from unittest import mock
 
 import splinter
@@ -8,17 +9,25 @@ from django.core import mail
 from django.urls import reverse
 
 
+@dataclass
+class UserInfo:
+    username: str
+    password1: str
+    password2: str
+    email: str
+
+
 class NewRegisteredUserMailTests(test.TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.client = test.Client()
         cls.url = reverse("register")
 
-    def fill_and_send_form(self, browser, username, password1, password2, email):
-        browser.fill("username", username)
-        browser.fill("password1", password1)
-        browser.fill("password2", password2)
-        browser.fill("email", email)
+    def fill_and_send_form(self, browser, user_info: UserInfo):
+        browser.fill("username", user_info.username)
+        browser.fill("password1", user_info.password1)
+        browser.fill("password2", user_info.password2)
+        browser.fill("email", user_info.email)
         browser.find_by_id("register-form").click()
 
     # TODO: not sure why this mock is needed but VCR tests fail if not here
@@ -31,12 +40,15 @@ class NewRegisteredUserMailTests(test.TestCase):
 
             self.assertEqual(auth.get_user_model().objects.count(), 0)
 
-            self.fill_and_send_form(
-                browser=browser,
+            user_info = UserInfo(
                 username="fake_user",
                 password1="fake_password",
                 password2="fake_password",
                 email="foo@bar.qux",
+            )
+            self.fill_and_send_form(
+                browser=browser,
+                user_info=user_info,
             )
 
         self.assertEqual(auth.get_user_model().objects.count(), 1)
